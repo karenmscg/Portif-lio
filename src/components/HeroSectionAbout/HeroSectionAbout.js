ccadbbdfca3efa6991ccbfe630713f4b5b5382d8
karenmscg <karen.myllena@gmail.com>
2021-08-11 17:10:54 -0300

    Align fixes graduation and technologies

--- src/components/HeroSectionAbout/HeroSectionAbout.js
+++ src/components/HeroSectionAbout/HeroSectionAbout.js
@@ -101,10 +101,12 @@ export const GraduationContent = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  align-items: center;
 `;
 
 export const TechnologiesContent = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  align-items: center;
 `;
